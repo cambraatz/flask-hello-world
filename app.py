4729938eebc4d3fd517b4157dac3b99da0da4d93
cambraatz <cabr8691@colorado.edu>
2024-03-25 18:55:14 +0000

    attempt to push db connection change

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World from Cameron Braatz in 3308!'
+
+@app.route('db_test')
+def db_test():
+    conn = psycopg2.connect("postgres://helloworld_user:PrD3vf6KbhHx8E7jz8Kc2mLZZImgVyMz@dpg-co0s7oect0pc73fkttc0-a/helloworld_db_4f3d")
+    conn.close()
+    return "Database Connection Successful"
